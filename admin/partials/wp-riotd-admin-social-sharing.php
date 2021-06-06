0894de0b89c8b8b05115ce68d9db3b9edde90729
Tommaso D'Argenio <tom@tommasodargenio.com>
2021-06-06 17:03:48 +0100

    changed styling of social table

--- admin/partials/wp-riotd-admin-social-sharing.php
+++ admin/partials/wp-riotd-admin-social-sharing.php
@@ -1,11 +1,14 @@
 <div id="social_sharing_box">
-<hr/>
-Like the plugin? Spread the love!<br/><br/>
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Display a random image from a reddit channel of your choice on your WordPress website with Reddit-IOTD" data-url="https://wordpress.org/plugins/reddit-iotd/" data-via="tommasodargenio" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<a href="https://twitter.com/tommasodargenio?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @tommasodargenio</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
-<script type="IN/Share" data-url="https://wordpress.org/plugins/reddit-iotd/"></script>
-<a target='_blank' href='https://www.reddit.com/submit?url=https://wordpress.org/plugins/reddit-iotd/'>
-<img src='<?php echo plugin_dir_url( __DIR__ ); ?>images/reddit-share-button.svg' alt='Reddit' width="60px" height="20px" style="vertical-align:bottom"/></a>
-
+    <hr/>
+    Like the plugin? Spread the love!<br/><br/>
+    <table>
+        <tbody>
+            <tr>
+                <td style="padding-top:4px"><a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Display a random image from a reddit channel of your choice on your WordPress website with Reddit-IOTD" data-url="https://wordpress.org/plugins/reddit-iotd/" data-via="tommasodargenio" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></td>
+                <td style="padding-top:4px"><a href="https://twitter.com/tommasodargenio?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @tommasodargenio</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></td>
+                <td><script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url="https://wordpress.org/plugins/reddit-iotd/"></script></td>
+                <td><a target='_blank' href='https://www.reddit.com/submit?url=https://wordpress.org/plugins/reddit-iotd/'><img src='<?php echo plugin_dir_url( __DIR__ ); ?>images/reddit-share-button.svg' alt='Reddit' width="60px" height="20px" style="vertical-align:bottom"/></a></td>
+            </tr>
+        </tbody>    
+    </table>
 </div>
