c4fc6f2d20bf8862f1de651e026429add3228a3a
Tommaso D'Argenio <tom@tommasodargenio.com>
2021-06-10 14:59:00 +0100

    added reset settings button

--- admin/partials/wp-riotd-admin-menu.php
+++ admin/partials/wp-riotd-admin-menu.php
@@ -1,6 +1,6 @@
 <div class="wrap">
     <div id="icon-themes" class="icon32"></div>
-    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>    
     <?php if ( isset($_GET['settings-updated']) && $_GET['settings-updated'] ) {?>
         <div class="notice notice-success is-dismissible">
             <p>Your settings have been updated!</p>
@@ -14,7 +14,7 @@
     <h2 class="nav-tab-wrapper">
         <?php $this->do_tabs($active_tab); ?>
     </h2>
-
+    
     <form method="POST" action="options.php">
         <?php 
             if ( $active_tab == $this->default_setting_tab ) {
@@ -24,8 +24,19 @@
             } else {
                 settings_fields( $active_tab );
                 do_settings_sections( $active_tab );
-                submit_button( __( 'Save Settings', 'wp_riotd' ) );    
-            }
-        ?>
+
+                if ( $active_tab == 'wp_riotd_section_general') {
+                    $this->do_cache();
+                }
+                ?>
+                <table class="form-table" role="presentation" style="width:5%">
+                    <tr>
+                        <td><?php submit_button( __( 'Save Settings', 'wp_riotd' ) ); ?></td>
+                        <td>
+                            <?php submit_button( __( 'Reset All Settings', 'wp_riotd'), 'secondary', 'reset', true, 'title="'.__('Click to reset all settings to default', 'wp_riotd').'"'); ?>
+                        </td>
+    <?php  }  ?>
+                    </tr>
+                </table>
     </form>
 </div>
