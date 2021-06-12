905a38766c1b77dc836a9c92c70dc29c87760ea1
Tommaso D'Argenio <tom@tommasodargenio.com>
2021-06-12 22:04:38 +0100

    added control if elements exists

--- public/js/wp-riotd-public.js
+++ public/js/wp-riotd-public.js
@@ -10,35 +10,40 @@ function wp_riotd_image_link(url) {
 
 function wp_riotd_image_fullscreen() {    
     var box = document.getElementById("reddit-iotd-full-size");
-    box.style.display = "block";
-
+    if (box != null) {
+        box.style.display = "block";
+    }
       var full_img = document.getElementById("ig-iotd-full");
-
-      if (full_img.clientWidth > box.clientWidth) {
-          var resized = full_img.clientWidth;
-          //while ( resized >= box.clientWidth ) {
-            resized = full_img.clientWidth - (box.clientWidth * .15);
-          //}
-          full_img.style.width = resized.toString() + "px";          
-      }
-      if (full_img.clientHeight > box.clientHeight) {
-          var resized = full_img.clientHeight;
-          //while( resized >= box.clientHeight ) {
-            resized = full_img.clientHeight - (box.clientHeight * .15);
-          //}
-          full_img.style.height = resized.toString() + "px";          
-      }
-    
+      if (full_img != null && box != null ) { 
+        if (full_img.clientWidth > box.clientWidth) {
+            var resized = full_img.clientWidth;
+            //while ( resized >= box.clientWidth ) {
+                resized = full_img.clientWidth - (box.clientWidth * .15);
+            //}
+            full_img.style.width = resized.toString() + "px";          
+        }
+        if (full_img.clientHeight > box.clientHeight) {
+            var resized = full_img.clientHeight;
+            //while( resized >= box.clientHeight ) {
+                resized = full_img.clientHeight - (box.clientHeight * .15);
+            //}
+            full_img.style.height = resized.toString() + "px";          
+        }
+    }    
 }
 
 function wp_riotd_image_close() {
     var box = document.getElementById("reddit-iotd-full-size");
-    box.style.display = "none";   
+    if ( box != null ) {
+        box.style.display = "none";  
+    }
 }
 
 document.addEventListener("click", function(e) {
     var box = document.getElementById("reddit-iotd-full-size");
-    if (!closest(e.target, box)) {
-        box.style.display = "none";        
+    if ( box != null ) {
+        if (!closest(e.target, box)) {
+            box.style.display = "none";        
+        }    
     }
 });
